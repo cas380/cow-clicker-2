83f1592ea2a05ce25f96acc83d6a0fa0237f9366
cas380 <57329598+cas380@users.noreply.github.com>
2020-02-23 20:32:51 -0500

    Figuring out custom users
    Form action is different, routes to custom place

--- main.py
+++ main.py
@@ -4,29 +4,28 @@ from User import User
 
 app = Flask(__name__)
 
-
 @app.route('/cowclicker.html')
 def root():
-  # use render_template to convert the template code to HTML.
-  # this function will look in the templates/ folder for your file.
-
-  username = request.form['username']
-  password = request.form['password']
-  user = User(username, password)
-
-  entity = test_grab(user) # user's data
-  if not entity:
-    test_put(user, 0, 0) # saves zeroes for new game
   entity = test_grab(user) # user's data for sure (needed to start game)
+  return render_template('cowclicker.html', page_title='Index Title Python Variable hehehaha cow', init_points=entity['points'])
 
-  return render_template('cowclicker.html', page_title='Index Title Python Variable hehehaha cow', init_points=0)
+  
+@app.route('/to-cow-game', methods=['POST'])
+def signin():
+    username = flask.request.form.get('username')
+    password = flask.request.form.get('password')
+    user = User(username, password)
 
+    entity = test_grab(user) # user's data
+    if not entity:
+      test_put(user, 0, 0) # saves zeroes for new game
+    entity = test_grab(user) # user's data for sure (needed to start game)
+
+    return flask.redirect('/cowclicker.html')
 
 @app.route('/')
 @app.route('/login.html')
 def login():
-  # request form allows python to obtain data from Form in html
-
   # put this into database, will read up on stuff
   return render_template('login.html', page_title='Save your work dude')
   
