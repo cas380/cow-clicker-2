61f6b76ab384e2ff7095996f8229f417ba529fcd
cas380 <57329598+cas380@users.noreply.github.com>
2020-02-23 21:29:26 -0500

    session stuff set up??

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session
+# RuntimeError: The session is unavailable because no secret key was set. 
+# Set the secret_key on the application to something unique and secret.
 import flask
 from DataStore import test_put, test_grab
 from User import User
 
 app = Flask(__name__)
-
+session = Session()
 
 @app.route('/cowclicker.html')
 def root():
-  #session is not defined according to google cloud
-  user = flask.session.get('user')
+  user = session.get('user')
   entity = test_grab(user) # user's data for sure (needed to start game)
   #points = request.form["points"] #json get points from js 
   return render_template('cowclicker.html', page_title='Index Title Python Variable hehehaha cow', init_points=entity['points'])
@@ -26,7 +27,7 @@ def signin():
       test_put(user, 0, 0) # saves zeroes for new game
     entity = test_grab(user) # user's data for sure (needed to start game)
 
-    flask.session['user'] = user
+    session['user'] = user
     return flask.redirect('/cowclicker.html')
 
 
@@ -42,4 +43,8 @@ def login():
   return render_template('login.html', page_title='Save your work dude')
   
 if __name__ == '__main__':
+    app.secret_key = '???'
+    app.config['SESSION_TYPE'] = 'filesystem'
+    session.init_app(app)
+
     app.run(host='127.0.0.1', port=8080, debug=True)
