31559b5faf90a60225f8366c6e223f407db473bf
cas380 <57329598+cas380@users.noreply.github.com>
2020-02-23 19:50:27 -0500

    Adding datastore
    implementing users, save data

--- DataStore.py
+++ DataStore.py
@@ -1,12 +1,32 @@
 from google.cloud import datastore
+from points import points
 
-def test_put():
+POINTS = 'Points'
+
+def test_put(user, pointsSave, cowSave):
     client = datastore.Client()
     
     kind = 'UserEntityType' # Enum!
-    name = 'uniqueID' # Unique identifier for key...
+    name = str(user) # Unique identifier for key... our user
     
     task_key = client.key(kind, name) # We have a key
     task = datastore.Entity(key=task_key) # We have an empty datastore entity
-    task['description'] = 'Buy milk' # Fill the empty entity
-    client.put(task) # Put!!!!!!!!!!
+
+    # Fill the empty entity
+    task['points'] = pointsSave 
+    task['cows'] = cowSave
+
+    client.put(task) # Put!!!
+
+#Returns NoneType if user doesn't exist
+def test_grab(user):
+    client = datastore.Client()
+
+    kind = 'UserEntityType' # Enum!
+    name = str(user) # Unique identifier for key... our user
+    
+    task_key = client.key(kind, name) # We have a key
+    entity = client.get(key) # We have the entity for this key
+    
+    print(str(entity))
+    return entity
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request
-from DataStore import test_put
+from DataStore import test_put, test_grab
 from User import User
 
 app = Flask(__name__)
@@ -9,19 +9,24 @@ app = Flask(__name__)
 def root():
   # use render_template to convert the template code to HTML.
   # this function will look in the templates/ folder for your file.
-  return render_template('cowclicker.html', page_title='Index Title Python Variable hehehaha cow')
+
+  username = request.form['username']
+  password = request.form['password']
+  user = User(username, password)
+
+  entity = test_grab(user) # user's data
+  if not entity:
+    test_put(user, 0, 0) # saves zeroes for new game
+  entity = test_grab(user) # user's data for sure (needed to start game)
+
+  return render_template('cowclicker.html', page_title='Index Title Python Variable hehehaha cow', init_points=entity['points'])
 
 
 @app.route('/')
 @app.route('/login.html')
 def login():
-  test_put()
+  # request form allows python to obtain data from Form in html
 
-  #request form allows python to obtain data from Form in html
-  #username = request.form['username']
-  #password = request.form['password'] No form yet
-  #user = User(username, password)
- 
   # put this into database, will read up on stuff
   return render_template('login.html', page_title='Save your work dude')
   
