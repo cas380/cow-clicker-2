2651396b27a0f5b6eae4442e0259c619a68f48b5
Kenny Meier <kem243@pitt.edu>
2020-03-28 20:26:24 -0400

    what if it does?

--- static/cowclicker_logic.js
+++ static/cowclicker_logic.js
@@ -234,9 +234,7 @@ function buyThatCow(cowID) {
 
     switch(cowID){
         case 1:
-            alert(cows);
             if(points >= 5000){
-                alert(cows & 2);
                 enoughpoints = 1;               // the user has enough points to buy this cow
                 if((cows & 2) != 2){
                     alreadyown = 1;             // the user does not already own this cow
@@ -247,9 +245,7 @@ function buyThatCow(cowID) {
             break;
         
         case 2:
-            alert(cows);
             if(points >= 10000){
-                alert(cows & 4);
                 enoughpoints = 1;               // the user has enough points to buy this cow
                 if((cows & 4) != 4){
                     alreadyown = 1;             // the user does not already own this cow
@@ -260,9 +256,7 @@ function buyThatCow(cowID) {
             break;
 
         case 3:
-            alert(cows);
             if(points >= 50000){
-                alert(cows & 8);
                 enoughpoints = 1;               // the user has enough points to buy this cow
                 if((cows & 8) != 8){
                     alreadyown = 1;             // the user does not already own this cow
@@ -273,9 +267,7 @@ function buyThatCow(cowID) {
             break;
 
         case 4:
-            alert(cows);
             if(points >= 100000){
-                alert(cows & 16);
                 enoughpoints = 1;               // the user has enough points to buy this cow
                 if((cows & 16) != 16){
                     alreadyown = 1;             // the user does not already own this cow
@@ -353,10 +345,10 @@ function buyThatCow(cowID) {
             var textnode = document.createTextNode("You do not have enough points to buy Minecraft Cow!");
         }
     }
-    /* 
+    
     node.appendChild(textnode);
     node.classList.add("logItem");
-    if(case == 0){
+    if(Case == 0){
         node.classList.add("red");
     }
     else{
@@ -367,5 +359,5 @@ function buyThatCow(cowID) {
         logger.removeChild(logger.childNodes[0]);									// Remove the oldest node once the limit has been reached
     }
     logger.appendChild(node);														// Append <p> to the logger
-    logger.scrollTop = logger.scrollHeight;*/
+    logger.scrollTop = logger.scrollHeight;
 }
