3848f516c5a455840ef33f3f9579b7abbc956b19
Kenny Meier <kem243@pitt.edu>
2020-04-19 14:01:24 -0400

    username Work

--- main.py
+++ main.py
@@ -70,7 +70,7 @@ def game():
   entity = test_grab(user) # user's data for sure (needed to start game)
 
   if entity: # shouldn't ever be empty??????
-    return render_template('cowclicker.html', init_points=entity['points'], init_cows=entity['cows'])
+    return render_template('cowclicker.html', init_points=entity['points'], init_cows=entity['cows'], user=entity['user'])
   else:
     return render_template('cowclicker.html', init_points=0, init_cows=1)
 
